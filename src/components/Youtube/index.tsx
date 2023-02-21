c70647b86ac5b8e5902e8a7147b3cdd0bda6445e
Tiago Cunha <tiago92cunha@gmail.com>
2023-02-21 23:26:35 +0000

    Test if rules is working

--- src/components/Youtube/index.tsx
+++ src/components/Youtube/index.tsx
@@ -1,23 +1,24 @@
-import { memo } from "react";
+import { memo } from 'react';
 
 type YoutubeStreamProps = {
-  url: string;
-  id: string;
+	url: string;
+	id: string;
 };
+//teste
 
 const Youtube: React.FC<YoutubeStreamProps> = ({ url, id }) => {
-  return (
-    <iframe
-      width="100%"
-      height="100%"
-      src={`https://www.youtube.com/embed/${url}`}
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      id={id}
-    ></iframe>
-  );
+	return (
+		<iframe
+			width="100%"
+			height="100%"
+			src={`https://www.youtube.com/embed/${url}`}
+			title="YouTube video player"
+			frameBorder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowFullScreen
+			id={id}
+		></iframe>
+	);
 };
 
 export const YoutubeStream = memo(Youtube);
